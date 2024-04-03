20c436dd7f84c8ccf1fee6924e3c08cc288b1e07
Mohaiminul Islam <mmislam272@gmail.com>
2024-04-03 16:25:02 +0600

    checkpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
+      <div className="z-10 p-4 text-black bg-gray-200 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        Here You go
+
       </div>
     </main>
   );
